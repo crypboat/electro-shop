54b7aa623b652b416b03e11a7524ca5e487caef9
crypboat <168261896+crypboat@users.noreply.github.com>
2026-05-08 17:38:50 +0600

    Update script.js

--- script.js
+++ script.js
@@ -140,8 +140,7 @@ function renderProducts() {
 
     card.innerHTML = `
       <div class="product-img-wrap">
-        <div class="product-emoji-img">
-        ${p.image ? '<img src="${p.image}" style="width:100%; height:100%; object-fit:cover; border-radius:10px;">' : p.emoji}</div>
+        <div class="product-emoji-img">${p.image ? '<img src="${p.image}" style="width:100%;height:100%;object-fit:cover;">' : p.emoji}</div>
         <div class="product-emoji-img">
         ${p.badge ? `<div class="product-badge">${p.badge}</div>` : ''}
         <div class="select-check ${isSelected ? 'checked' : ''}" onclick="toggleSelect(${p.id})" title="Select for bulk order">
